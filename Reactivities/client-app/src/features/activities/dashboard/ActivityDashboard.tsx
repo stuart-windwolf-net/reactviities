9701f3288ee8145ba8380c7c625a081306d011b4
Stuart Markus <stuart@windwolf.net>
2022-06-18 18:47:37 +1000

    End of section 9

--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,9 +1,10 @@
 import { observer } from "mobx-react-lite";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layouts/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
-import ActivityList from "./ActivityList";
+import ActivityList from "../details/ActivityList";
+import ActivityFilters from "./ActivityFilters";
 
 export default observer(function ActivityDashboard() {
     const {activityStore} = useStore();
@@ -20,7 +21,7 @@ export default observer(function ActivityDashboard() {
                 <ActivityList />
              </Grid.Column>
              <Grid.Column width={6}>
-                <h2>Activity Filters</h2>
+                <ActivityFilters />
              </Grid.Column>
         </Grid>
     )
--- Reactivities/client-app/src/index.tsx
+++ Reactivities/client-app/src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-calendar/dist/Calendar.css'
 import './app/layouts/styles.css';
 import App from './app/layouts/App';
 import reportWebVitals from './reportWebVitals';
